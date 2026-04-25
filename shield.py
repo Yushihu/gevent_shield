5c3e32b02c4cef68069524102b505314e909703b
yushi <yushihu0412@163.com>
2026-04-26 00:33:29 +0800

    add support for generator

--- shield.py
+++ shield.py
@@ -1,28 +1,176 @@
-from gevent import GreenletExit, spawn, getcurrent, Greenlet
+
+from inspect import isgeneratorfunction
 from functools import wraps
+from typing import Callable
+
+from gevent import GreenletExit, spawn, getcurrent, Greenlet
+from gevent.event import Event
 
 
-def shield(et=GreenletExit, suppress=False):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            g = spawn(func, *args, **kwargs)
-            signal = None
-            while True:
-                try:
-                    g.join()
-                    break
-                except et as e:
-                    signal = e
-                    continue
-
-            if signal is not None and not suppress:
-                cur = getcurrent()
-                if isinstance(cur, Greenlet):
-                    cur.kill(signal, block=False)
-
-            return g.get()
-
+def shield(exception_type=GreenletExit, suppress=False):
+    def decorator(func: Callable[...]) -> Callable[...]:
+        if isgeneratorfunction(func):
+            @wraps(func)
+            def wrapper(*args, **kwargs):
+                yield from GeneratorShield(exception_type, suppress).execute(func, *args, **kwargs)
+        else:
+            @wraps(func)
+            def wrapper(*args, **kwargs):
+                return FunctionShield(exception_type, suppress).execute(func, *args, **kwargs)
+            
         return wrapper
 
     return decorator
+
+
+def shielding(exception_type=GreenletExit, suppress=False):
+    def d(func):
+        return FunctionShield(exception_type, suppress).execute(func)
+    return d
+
+
+class _Shield:
+    def __init__(self, exception_type=GreenletExit, suppress=False):
+        self.exception_type = exception_type
+        self.suppress = suppress
+
+    def execute(self, func, *args, **kwargs):
+        raise NotImplementedError
+
+
+class FunctionShield(_Shield):
+    def execute(self, func, *args, **kwargs):
+        reraise = not self.suppress
+        
+        exception = None
+
+        def wrapper():
+            nonlocal exception
+            try:
+                return func(*args, **kwargs)
+            except Exception as e:
+                exception = e
+                return
+
+        g = spawn(wrapper)
+
+        try:
+            g.join()
+        except self.exception_type as e:
+            if reraise:
+                signals = []
+                while True:
+                    try:
+                        g.join()
+                        break
+                    except self.exception_type as e:
+                        signals.append(e)
+                        continue
+
+                cur = getcurrent()
+                if isinstance(cur, Greenlet):
+                    for signal in signals:
+                        cur.kill(signal, block=False)
+            else:
+                while True:
+                    try:
+                        g.join()
+                        break
+                    except self.exception_type as e:
+                        continue
+
+        if exception:
+            raise exception
+
+        return g.get()
+
+
+class GeneratorShield(_Shield):
+    def execute(self, func, *args, **kwargs):
+        in_ = Event()
+        out = Event()
+
+        yielded = None
+        received = None
+
+        done = False
+        exception = None
+        closed = False
+
+        reraise = not self.suppress
+
+        def inner():
+            nonlocal yielded, received, done, exception, closed
+            generator = func(*args, **kwargs)
+
+            try:
+                try:
+                    yielded = next(generator)
+                except StopIteration:
+                    return
+
+                while True:
+                    out.set()
+                    in_.wait()
+                    in_.clear()
+                    if closed:
+                        generator.close()
+                        break
+
+                    try:
+                        yielded = generator.send(received)
+                    except StopIteration:
+                        break
+
+                    received = None
+            except Exception as e:
+                exception = e
+                return
+            finally:
+                out.set()
+                done = True
+
+        g = spawn(inner)
+
+        while True:
+            try:
+                out.wait()
+            except self.exception_type as e:
+                if reraise:
+                    signals = [e]
+
+                    while True:
+                        try:
+                            out.wait()
+                            break
+                        except self.exception_type as e:
+                            signals.append(e)
+                            continue
+
+                    cur = getcurrent()
+                    if isinstance(cur, Greenlet):
+                        for signal in signals:
+                            cur.kill(signal, block=False)
+
+                else:
+                    while True:
+                        try:
+                            out.wait()
+                            break
+                        except self.exception_type as e:
+                            continue
+
+            out.clear()
+            if done:
+                break
+
+            try:
+                received = yield yielded
+            except GeneratorExit:
+                closed = True
+            in_.set()
+
+        if exception:
+            raise exception
+
+        g.get()
---
+++ test.py
@@ -0,0 +1,188 @@
+from gevent import GreenletExit, sleep, spawn
+
+from shield import shield
+
+
+def titled(func):
+    def wrapper(*args, **kwargs):
+        print(f"=== {func.__name__} ===")
+        return func(*args, **kwargs)
+    return wrapper
+
+
+@titled
+def test1():
+    count = 0
+
+    def inc():
+        nonlocal count
+        count += 1
+        print(f"inc: {count}")
+
+    @shield()
+    def shield_inc():
+        nonlocal count
+        print("enter shield")
+        sleep(0.2)
+        inc()
+        print("exit shield")
+
+
+    def main():
+        nonlocal count
+        print(f"enter main: {count}")
+        shield_inc()
+        try:
+            sleep(0)
+            inc()
+        except GreenletExit:
+            print("killed in main")
+            raise
+        finally:
+            print(f"finally in main: {count}")
+
+
+    def kill(g):
+        sleep(0.1)
+        print("kill")
+        g.kill()
+
+    g = spawn(main)
+    spawn(kill, g)
+    spawn(kill, g)
+    g.join()
+
+    print(count)
+
+    assert count == 1
+
+
+@titled
+def test2():
+    class MyExit(GreenletExit):
+        pass
+
+    count = 0
+
+    def inc():
+        nonlocal count
+        count += 1
+        print(f"inc: {count}")
+
+    @shield(exception_type=MyExit, suppress=True)
+    def shield_inc():
+        nonlocal count
+        print("enter shield")
+        sleep(0.2)
+        inc()
+        print("exit shield")
+
+
+    def main():
+        nonlocal count
+        print(f"enter main: {count}")
+        shield_inc()
+        try:
+            sleep(0)
+            inc()
+        except GreenletExit:
+            print("killed in main")
+            raise
+        finally:
+            print(f"finally in main: {count}")
+
+
+    def kill(g):
+        sleep(0.1)
+        print("kill")
+        g.kill(MyExit())
+
+    g = spawn(main)
+    spawn(kill, g)
+    g.join()
+
+    print(count)
+
+    assert count == 2
+
+
+@titled
+def test3():
+    class TheError(Exception):
+        pass
+
+    @shield()
+    def f():
+        e = TheError("test")
+        print(f"raise error {e}")
+        raise e
+
+    try:
+        f()
+        assert False, "should raise"
+    except TheError as e:
+        print(f"caught error: {e}")
+
+
+@titled
+def test4():
+    @shield()
+    def create_str(ret):
+        print(f"creating string: {ret}")
+        return ret
+    
+    expected = "hello"
+    s = create_str(expected)
+    print(f"created string: {s}")
+    assert s == expected
+
+
+@titled
+def test5():
+    count = 0
+
+    @shield()
+    def shield_func(i):
+        try:
+            while True:
+                print(f"enter shield: {i}")
+                i += 1
+                sleep(0.1)
+                print(f"exit shield: {i}")
+                i = yield i
+                print(f"received in shield: {i}")
+        finally:
+            print(f"finally in shield: {i}")
+
+    def main():
+        nonlocal count
+        g = shield_func(count)
+        i = next(g)
+        while True:
+            print(f"main received: {i}")
+            count = i
+            try:
+                i = g.send(i + 1)
+                sleep(0)
+            except StopIteration:
+                print("shield_func done")
+                break
+
+    def kill(gx):
+        sleep(0.25)
+        print("kill")
+        gx.kill()
+    
+    g = spawn(main)
+    spawn(kill, g)
+    g.join()
+
+    assert count == 3
+
+
+if __name__ == "__main__":
+    test1()
+    test2()
+    test3()
+    test4()
+    test5()
